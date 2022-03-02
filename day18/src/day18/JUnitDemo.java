890e4a144cad85491f82c8868ab67917f4be7c01
HP <HP@LAPTOP-MBU8NU9S>
2022-03-02 22:31:03 +0530

    JUnit

---
+++ day18/src/day18/JUnitDemo.java
@@ -0,0 +1,19 @@
+package day18;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class JUnitDemo {
+
+@Test
+	void test() {
+		System.out.println("Welcome");
+	}
+
+@Disabled
+	
+@Test
+	void test1() {
+		System.out.println("Hello");
+	}
+}
