716b20c0b7b536b11488077c0b68a26747ec8bec
HP <HP@LAPTOP-MBU8NU9S>
2022-03-08 09:20:00 +0530

    java feature

---
+++ day21/src/day21/feature1.java
@@ -0,0 +1,21 @@
+package day21;
+
+import day21.javafeature.Actor;
+
+public class feature1 {
+	
+	public class villan implements Actor{
+		public void act() {
+			System.out.println("i can act");
+		}
+		public void speak() {
+			System.out.println("i can speak");
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ day21/src/day21/javafeature.java
@@ -0,0 +1,14 @@
+package day21;
+
+public class javafeature {
+	
+	public interface Actor{
+		void act();
+		void speak() {
+			System.out.println("Using Default we can give mothod body to interface class");
+		}
+	}
+}
+
+
+	
